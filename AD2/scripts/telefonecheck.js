85e114067a83c9a649c75f2d88ecc32debf2e4ff
Sabathick <erickick13@gmail.com>
2021-12-02 13:59:58 -0300

    updating pedidos

--- AD2/scripts/telefonecheck.js
+++ AD2/scripts/telefonecheck.js
@@ -18,3 +18,33 @@ function VerificaTelef( campo, tam ) {
     }
     return true;
    }
+
+   /*
+* Lista de Compras
+*/
+var tabPrcProd = [ [ "", 0],
+[ "Iogurte", 8],
+[ "Iogurte", 8],
+[ "Balas", 6],
+[ "Balas", 6],
+[ "Barra", 5],
+["Pote", 20],
+["Pote", 25],
+["Pote", 40],
+["Própolis", 16],
+["Própolis", 9] ];
+function IncluiLista( sel ) {
+var prod = sel.selectedIndex;
+if ( prod != 0 ) {
+var tip = tabPrcProd[prod][0];
+var prc = tabPrcProd[prod][1];
+sel.selectedIndex = 0;
+with ( sel.form ) {
+listaPedidos.value += tip + " " +
+sel.options[prod].text + '\n';
+TxtTotal.value = prc + parseInt(TxtTotal.value);
+}
+}
+else
+alert("Nenhum Produto selecionado!");
+}
